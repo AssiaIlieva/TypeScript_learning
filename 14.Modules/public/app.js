22f177a2e97bdcbe022c2e981d956d6c288ada7a
AssiaIlieva <ac17@abv.bg>
2024-04-23 15:55:58 +0300

    Added an item in TypeScript Learning
    Theme 14. Modules

---
+++ 14.Modules/public/app.js
@@ -0,0 +1,9 @@
+import { Invoice } from './classes/invoice.js';
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
