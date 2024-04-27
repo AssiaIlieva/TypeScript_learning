0f15b3f51737a9887a5844065a31be1377010c83
AssiaIlieva <ac17@abv.bg>
2024-04-27 08:55:41 +0300

    Added a solution in Introduction to TypeScript
    The .js and .js.map files of the second solution to Task 2.Largest Number

---
+++ TypeScript-SoftUni-Course-Apr24/1.Introduction to TypeScript/out/2.largestNumber2.js
@@ -0,0 +1,7 @@
+"use strict";
+function largestNum(a, b, c) {
+    let result = Math.max(a, b, c);
+    console.log(`The largest number is ${result}.`);
+}
+largestNum(5, -3, 16);
+//# sourceMappingURL=2.largestNumber2.js.map
