c4b3b6d9c26a451f5f7144ea041b069be72da589
AssiaIlieva <ac17@abv.bg>
2024-04-29 20:59:10 +0300

    Added a solution in Introduction to TypeScript
    The .ts file to Task 3. Sum of Numbers

---
+++ TypeScript-SoftUni-Course-Apr24/1.Introduction to TypeScript/3.sumOfNumbers.ts
@@ -0,0 +1,13 @@
+function sumOfNumbers(a:string, b:string){
+    const num1 = Number(a);
+    const num2 = Number(b);
+    let sum = 0;
+
+    for(let i = num1; i <= num2; i++){
+        sum += i;
+    }
+
+    console.log(sum);
+    
+}
+sumOfNumbers('-8', '20')
