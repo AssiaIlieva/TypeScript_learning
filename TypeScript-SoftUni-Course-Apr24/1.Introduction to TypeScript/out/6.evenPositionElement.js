122f448554de016b5284d7606f6a16d21f297e74
AssiaIlieva <ac17@abv.bg>
2024-05-02 23:22:12 +0300

    Added a solution in Introduction to TypeScript
    The .js file of the first solution to Task 6. Even position element

---
+++ TypeScript-SoftUni-Course-Apr24/1.Introduction to TypeScript/out/6.evenPositionElement.js
@@ -0,0 +1,10 @@
+"use strict";
+function evenPositionElement(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i += 2) {
+        result.push(array[i]);
+    }
+    console.log(result.join(' '));
+}
+evenPositionElement(['20', '30', '40', '50', '60']);
+//# sourceMappingURL=6.evenPositionElement.js.map
