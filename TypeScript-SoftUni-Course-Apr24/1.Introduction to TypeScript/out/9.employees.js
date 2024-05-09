5e41328cc7c6af5e982caaa4086e661612876002
AssiaIlieva <ac17@abv.bg>
2024-05-09 10:15:01 +0300

    Added a solution in Introduction to TypeScript
    Created 9. Employees.js

---
+++ TypeScript-SoftUni-Course-Apr24/1.Introduction to TypeScript/out/9.employees.js
@@ -0,0 +1,13 @@
+"use strict";
+function employees(arr) {
+    for (let employee of arr) {
+        console.log(`Name: ${employee} -- Personal Number: ${employee.length}`);
+    }
+}
+employees([
+    'Samuel Jackson',
+    'Will Smith',
+    'Bruce Willis',
+    'Tom Holland'
+]);
+//# sourceMappingURL=9.employees.js.map
