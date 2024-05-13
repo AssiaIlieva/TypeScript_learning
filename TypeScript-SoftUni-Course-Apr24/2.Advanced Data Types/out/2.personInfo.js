d68f133eb79696d786dfc6c6f14ff1eed5e47e7c
AssiaIlieva <ac17@abv.bg>
2024-05-13 16:31:22 +0300

    Added a solution in Advanced Data Types
    The .js file of Task 2. Person Info

---
+++ TypeScript-SoftUni-Course-Apr24/2.Advanced Data Types/out/2.personInfo.js
@@ -0,0 +1,11 @@
+"use strict";
+const getPersonInfo = (firstName, lastName, age) => {
+    const personInfo = {
+        firstName: firstName,
+        lastName: lastName,
+        age: Number(age),
+    };
+    return personInfo;
+};
+console.log(getPersonInfo("Peter", "Pan", "20"));
+//# sourceMappingURL=2.personInfo.js.map
